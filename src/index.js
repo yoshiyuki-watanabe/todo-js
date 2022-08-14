846c41e340f049da3bb79db3218d4b002e1d24e6
yoshiyuki-watanabe <75032512+yoshiyuki-watanabe@users.noreply.github.com>
2022-08-14 13:34:51 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -5,24 +5,61 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  // p生成
+  // リスト生成
   const p = document.createElement("p");
   p.innerText = inputText;
 
-  // div生成
   const div = document.createElement("div");
   div.className = "list-row";
   div.appendChild(p);
 
-  // list生成
+  //完了ボタン
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //TODOリスト生成
+    const cP = document.createElement("p");
+    cP.innerText = inputText;
+
+    const cDiv = document.createElement("div");
+    cDiv.className = "list-row";
+    cDiv.appendChild(cP);
+
+    //戻すボタン
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻す";
+    returnButton.addEventListener("click", () => {
+      document.getElementById("incomplete-list").appendChild(li);
+      //完了リストから削除
+      returnButton.parentNode.parentNode.remove();
+    });
+    cDiv.appendChild(returnButton);
+
+    // 完了リストに追加
+    const cLi = document.createElement("li");
+    cLi.appendChild(cDiv);
+
+    document.getElementById("complete-list").appendChild(cLi);
+
+    //未完了リストから削除
+    completeButton.parentNode.parentNode.remove();
+  });
+  div.appendChild(completeButton);
+
+  //削除ボタン
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //未完了リストから削除
+    deleteButton.parentNode.parentNode.remove();
+  });
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
   const li = document.createElement("li");
   li.appendChild(div);
 
-  // 未完了リストに追加
-  document
-    .getElementById("incomplete-list")
-    .getElementsByTagName("li")[0]
-    .appendChild(li);
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document
